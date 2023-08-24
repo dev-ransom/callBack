c46a53a6479d08485a771a2dc2d5a7ff542c01a5
devransom-github <sundaystanley56@gmail.com>
2023-08-24 11:21:36 +0100

    using regular function

--- script.js
+++ script.js
@@ -14,45 +14,64 @@ const stocks = {
 // arrow function
 let is_shop_open = true;
 
-let order = (time, work) =>{
-    return new Promise((resolve, reject)=>{
+// let order = (time, work) =>{
+//     return new Promise((resolve, reject)=>{
+//         if(is_shop_open){
+//             setTimeout(()=>{
+//                 resolve(work())
+//             }, time)
+//         }else{
+//             reject(console.log('our shop is closed'))
+//         }
+        
+//     })
+// }
+// // main promises
+// order(2000, ()=> console.log(`${stocks.Fruits[0]} was selected`))
+
+// .then(()=>{
+//     return order(0, ()=> console.log('production has started'))
+// })
+
+// .then(() =>{
+//     return order(2000, ()=> console.log('The fruit has been chopped'))
+// })
+
+// .then(() =>{
+//     return order(1000, ()=> console.log(`${stocks.Liquids[0]} and ${stocks.Liquids[1]} were selected`))
+// })
+
+// .then(() =>{
+//     return order(1000, ()=> console.log('start the machine'))
+// })
+
+// .then(()=>{
+//     return order(2000, ()=> console.log(`The ice was placed on ${stocks.Holders[2]}`))
+// })
+
+// .then(()=> {
+//     return order(3000, ()=> console.log(`${stocks.Toppings[1]} was selected as toppings`))
+// })
+
+// .then(() =>{
+//     return order(2000, ()=> console.log('Serve the ice cream to the customer'))
+// })
+
+// regular function 
+let order = function(time, work){
+    return new Promise(function(resolve, reject){
         if(is_shop_open){
-            setTimeout(()=>{
+            setTimeout(function(){
                 resolve(work())
             }, time)
         }else{
             reject(console.log('our shop is closed'))
         }
-        
     })
 }
-// main promises
-order(2000, ()=> console.log(`${stocks.Fruits[0]} was selected`))
-
-.then(()=>{
-    return order(0, ()=> console.log('production has started'))
+// main promise
+order(2000, function(){
+    console.log(`${stocks.Fruits[2]}`)
 })
 
-.then(() =>{
-    return order(2000, ()=> console.log('The fruit has been chopped'))
-})
 
-.then(() =>{
-    return order(1000, ()=> console.log(`${stocks.Liquids[0]} and ${stocks.Liquids[1]} were selected`))
-})
-
-.then(() =>{
-    return order(1000, ()=> console.log('start the machine'))
-})
-
-.then(()=>{
-    return order(2000, ()=> console.log(`The ice was placed on ${stocks.Holders[2]}`))
-})
-
-.then(()=> {
-    return order(3000, ()=> console.log(`${stocks.Toppings[1]} was selected as toppings`))
-})
-
-.then(() =>{
-    return order(2000, ()=> console.log('Serve the ice cream to the customer'))
-})
